dc8bb6294fd30eefb46a276b095514b1eb2fef8a
ReptiloidAnunak <120068158+ReptiloidAnunak@users.noreply.github.com>
2022-12-14 00:03:14 -0300

    Add files via upload

---
+++ Artemiy_hw_9/classes.py
@@ -0,0 +1,13 @@
+class Candidate ():
+
+
+    def __init__(self, pk, name, picture, position, gender, age, skills):
+        self.pk = pk
+        self.name = name
+        self.picture = picture
+        self.position = position
+        self.gender = gender
+        self.age = age
+        self.skills = skills
+
+

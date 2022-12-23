520107349b9111280592f5cb8db0439f4daa9db8
ReptiloidAnunak <120068158+ReptiloidAnunak@users.noreply.github.com>
2022-12-23 00:13:24 -0300

    Add files via upload

---
+++ Atremiy_hw_10/candidate_app/main.py
@@ -0,0 +1,46 @@
+# Импорт необходимых функций
+from flask import Flask, render_template
+from utils import load_candidates_from_json, get_candidate, get_candidate_by_name, get_candidates_by_skill
+
+# Маршрутизация
+app = Flask(__name__)
+@app.route('/')
+def page_index():
+    candidates = load_candidates_from_json()
+    return render_template('list.html', candidates=candidates)
+
+
+@app.route('/candidates/<int:id>')
+def page_candidate(id):
+    candidate = get_candidate(id)
+    if candidate == None:
+        return '''<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Кандидат</title>
+</head>
+<body bgcolor="#20B2AA">
+    <br><br><br><br><br><br>
+    <center><h1>Кандидат с таким номером не зарегистрирован</h1></center>
+    <br>
+    <center><h2><a href='/'> Перейти<br>на Главную страницу </a></h2></center>
+</body>
+</html>'''
+    return render_template('card.html', candidate=candidate)
+
+
+@app.route('/search/<candidate_name>')
+def page_search(candidate_name):
+    candidates = get_candidate_by_name(candidate_name)
+    return render_template('search.html', candidates=candidates)
+
+
+@app.route('/skill/<skill_name>')
+def page_skill(skill_name):
+    candidates = get_candidates_by_skill(skill_name)
+    return render_template('skill.html', candidates=candidates)
+
+# Запуск кода
+app.run()
+
